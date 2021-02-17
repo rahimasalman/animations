4cd5fdeddbd3a2350a311c5cbb24247dce458f5d
Rahima Salmanova <64548507+rahimasalman@users.noreply.github.com>
2021-02-17 17:51:55 +0400

    Add files via upload

---
+++ story/script.js
@@ -0,0 +1,26 @@
+//Setup website here
+let hero = document.querySelector("#hero");
+let villain = document.querySelector("#villain");
+let lightning = document.querySelector("#lightning");
+
+//Setup animation code here
+let lightningStart = {"left": "290px"};
+let lightningEnd   = {"left": "750px"};
+let options        = {"duration": 800};
+
+let hitTheVillain = () => {
+  let villainStart = { 
+    transform: '',
+    opacity: 100
+  };
+  let villainEnd = {
+    transform: 'ease-out loop',
+    opacity: 0
+  };
+  let options = {"duration": 1500};
+  villain.animate([villainStart, villainEnd], options);
+}
+
+//Run animation code here
+lightning.animate([lightningStart, lightningEnd], options).onfinish = hitTheVillain; 
+
